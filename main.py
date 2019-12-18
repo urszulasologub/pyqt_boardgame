ed5b6f656ba36a0233bc1507dce1891d47607b04
Urszula Sołogub <gold.ula4@gmail.com>
2019-12-18 19:46:16 +0100

    Small interface changes

--- main.py
+++ main.py
@@ -4,75 +4,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtPrintSupport import *
-
-
-class PlayerInfo(QWidget):
-	def __init__(self, parent, player):
-		super(PlayerInfo, self).__init__(parent)
-		self.setStyleSheet("""
-				background-image: url(UI/brown_background.jpg);
-				background-attachment: scroll;
-				border: 2px outset gray;
-				border-radius: 10px;
-				color: white;
-				font-size: 18pt;
-				font: Arial;
-			""")
-		self.grid = QGridLayout(self)
-		castle_image = QPushButton()
-		castle_image.setStyleSheet('border-image: url(UI/castle_inside); min-height: 200px; ')
-		self.grid.addWidget(castle_image, 0, 0)
-
-		self.labels = [QLabel(player), QLabel('Poziom zamku: 1'), QLabel('Bohaterowie: '), QLabel('Jednostki bohatera: '), QLabel('Złoto: 0')]
-
-		i = 1
-		for label in self.labels:
-			self.grid.addWidget(label, i, 0)
-			i += 1
-
-		self.setLayout(self.grid)
-		self.update_castle_level(2)
-		self.update_gold_amount(5000)
-
-
-	def update_castle_level(self, level):
-		text = 'Poziom zamku: ' + str(level)
-		self.labels[1] = QLabel(text)
-		self.grid.addWidget(self.labels[1], 2, 0)
-				
-	
-	def update_gold_amount(self, amount):
-		text = 'Złoto: ' + str(amount)
-		self.labels[4] = QLabel(text)
-		self.grid.addWidget(self.labels[4], 5, 0)
-
-
-
-class PlayerActions(QWidget):
-	def __init__(self, parent, player):
-		super(PlayerActions, self).__init__(parent)
-		self.setStyleSheet("""
-				background-image: url(UI/brown_background.jpg);
-				background-attachment: scroll;
-				border: 2px outset gray;
-				border-radius: 10px;
-				color: white;
-				font-size: 18pt;
-				font: Arial;
-				min-height: 70px;
-			""")
-
-
-		self.grid = QGridLayout(self)
-		self.labels = ['Zobacz druzynę', 'Zakończ kolejkę', 'Atakuj', 'Bohater1', 'Bohater2', 'Bohater3']
-		self.buttons = []
-
-		for i in range(len(self.labels)):
-			self.buttons.append(QPushButton(self.labels[i]))
-			self.grid.addWidget(self.buttons[i], i, 0)
-
-			
-		self.setLayout(self.grid)
+from player import *
 
 
 
