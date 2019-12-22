65e4c2527ffb78f871fdff752fea74c7f8a2379b
Daria Lasecka <daria.las@wp.pl>
2019-12-22 19:58:30 +0100

    Added unit numbers and stats

--- castle.py
+++ castle.py
@@ -22,8 +22,17 @@ class Castle(QDialog):
 			""")
 
 		grid = QGridLayout(self)
+
+		self.level1 = self.parent().in_castle_units['level_1']
+		self.level2 = self.parent().in_castle_units['level_2']
+		self.level3 = self.parent().in_castle_units['level_3']
+		self.level4 = self.parent().in_castle_units['level_4']
+		self.level5 = self.parent().in_castle_units['level_5']
+
+		units = QLabel(
+			'Dostępne:\nJednostki poziomu 1: %d (0)\nJednostki poziomu 2: %d (0)\nJednostki poziomu 3: %d (0)\nJednostki poziomu 4: %d (0)\nJednostki poziomu 5: %d (0)\n'
+			% (self.level1, self.level2, self.level3, self.level4, self.level5))# nie wiem do czego odnosi się 0 w nawiasie, więc nic z tym nie robię xd
 		
-		units = QLabel('Dostępne:\nJednostki poziomu 1: 0 (0)\nJednostki poziomu 2: 0 (0)\nJednostki poziomu 3: 0 (0)\nJednostki poziomu 4: 0 (0)\nJednostki poziomu 5: 0 (0)\n')
 		grid.addWidget(units)
 		upgrade = QLabel('Ulepsz zamek za 3000 złota')
 		grid.addWidget(upgrade)
@@ -32,7 +41,3 @@ class Castle(QDialog):
 		for i in range(len(prices)):
 			if available[i]:
 				grid.addWidget(QPushButton('Kup jednostkę poziomu %s za %s złota' % (str(i + 1), str(prices[i]))), i + 2, 0)
-
-	def show_castle(self):
-		dialog = Castle(self)
-		dialog.exec_()
